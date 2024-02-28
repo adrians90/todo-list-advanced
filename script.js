3d88ab444a27952e4f77f5d4b4e97a2ec8a659f3
Adrian <adriansandu9090@gmail.com>
2024-02-28 16:47:56 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("#new-todo-form")
+const todoInput = document.querySelector("#todo-input")
+const list = document.querySelector("#list")
+const template = document.querySelector("#list-item-template")
+// Add todos
+    //user will type in todo and click  add todo button. This should add the todo
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    const todoName = todoInput.value
+    renderTodo(todoName)
+    todoInput.value = ""
+
+})
+
+function renderTodo(todoName) {
+    const templateClone = template.content.cloneNode(true)
+    const textElement = templateClone.querySelector("[data-list-item-text]")
+    textElement.innerText = todoName
+    list.appendChild(textElement)
+
+}
+    //to the list above.
+
+
+
+//Delete todos
+// Complete todos
+//Save todos
+//Load todos
